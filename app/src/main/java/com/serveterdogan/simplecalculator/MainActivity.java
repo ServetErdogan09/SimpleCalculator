44ffe763bf2bf30199cd6d760365c28517acba8f
Servet erdoğan <eservet224@gmail.com>
2024-02-11 17:14:40 +0300

    SimpleCalculator
    Bu proje, basit bir hesap makinesi uygulamasını içerir. Android Studio kullanılarak Java dilinde geliştirilmiştir.

---
+++ app/src/main/java/com/serveterdogan/simplecalculator/MainActivity.java
@@ -0,0 +1,107 @@
+package com.serveterdogan.simplecalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    EditText number1Text;
+    EditText number2Text;
+    TextView resultText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        number1Text = findViewById(R.id.numberTest1);
+        number2Text = findViewById(R.id.numberTest2);
+        resultText = findViewById(R.id.resultTest);
+
+    }
+
+    public void sum(View view){
+
+
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()){
+
+            resultText.setText("Enter Number!");
+        }else {
+
+            // aldığımız String değerini Integer çevirdik
+            int number1 =  Integer.parseInt(number1Text.getText().toString());
+            int number2 =  Integer.parseInt(number2Text.getText().toString());
+            int result = number1 + number2;
+
+            resultText.setText("Result : " + result);
+
+        }
+
+
+    }
+
+    public void deduct(View view) {
+
+
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()) {
+
+            resultText.setText("Enter Number!");
+        } else {
+
+            // aldığımız String değerini Integer çevirdik
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 - number2;
+
+            resultText.setText("Result : " + result);
+
+
+        }
+
+    }
+
+    public void multiply(View view) {
+
+
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()) {
+
+            resultText.setText("Enter Number!");
+        } else {
+
+            // aldığımız String değerini Integer çevirdik
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 * number2;
+
+            resultText.setText("Result : " + result);
+
+        }
+
+    }
+
+
+
+    public void  divide(View view) {
+
+        if (number1Text.getText().toString().isEmpty() || number2Text.getText().toString().isEmpty()) {
+
+            resultText.setText("Enter Number!");
+        } else {
+
+            // aldığımız String değerini Integer çevirdik
+            int number1 = Integer.parseInt(number1Text.getText().toString());
+            int number2 = Integer.parseInt(number2Text.getText().toString());
+            int result = number1 / number2;
+
+            resultText.setText("Result : " + result);
+
+
+        }
+    }
+
+}
